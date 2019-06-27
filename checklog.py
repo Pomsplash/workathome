56a07310b5a49077ac43540f3dee140fd269ae0f
Pom <passavich.lim@gmail.com>
2019-06-27 19:58:15 +0900

    Add collectlog function but not finish

--- checklog.py
+++ checklog.py
@@ -2,12 +2,15 @@ import os
 import sys
 from datetime import datetime,time,timedelta
 import glob
+import re
 
 DIR_NAME = '/var/log/syslog/*/*/*/*.log'
+SAVE_FILE = '/tmp/test'
 
 def main():
     date_list = dateconvert()
     filelog = filelist()
+    collectlog(date_list,filelog)
 
 def dateconvert():
     date_list = []
@@ -29,9 +32,20 @@ def filelist():
     for d in dir:
         filelog.append(d)
 
-    print(filelog)
+    #print(filelog)
     return filelog
 
+def collectlog(date_list,filelog):
+    r = re.compile(r'w*\caletas1\w*')
+    select = filter(r.search,filelog)
+    print(select)
+    
+    #for i in date_list:
+       # if re.match()
+        
+        
+
+    
 if __name__ == "__main__":
     startdate = sys.argv[1]
     enddate = sys.argv[2]
