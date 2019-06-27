341b0a1103bba761c733760cf689e69939cfb4d8
Pom <passavich.lim@gmail.com>
2019-06-27 18:00:10 +0900

    Add filelist function

--- checklog.py
+++ checklog.py
@@ -1,18 +1,36 @@
 import os 
 import sys
-from datetime import datetime
+from datetime import datetime,time,timedelta
+import glob
+
+DIR_NAME = '/var/log/syslog/*/*/*/*.log'
 
 def main():
-    print("test")
-    dateconvert()
+    date_list = dateconvert()
+    filelog = filelist()
 
 def dateconvert():
-    print(startdate)
-    print(enddate)
+    date_list = []
     startdate_convert = datetime.strptime(startdate,'%Y%m%d')
     enddate_convert = datetime.strptime(enddate,'%Y%m%d')
-    print(startdate_convert)
-    print(enddate_convert)
+    diffdate =  enddate_convert - startdate_convert
+    
+    for i in range(diffdate.days +1):
+        x = startdate_convert+timedelta(days=i)
+        y = x.strftime("%Y%m%d")
+        date_list.append(y)     
+        
+    print(date_list)
+    return date_list
+
+def filelist():
+    filelog = []
+    dir = glob.glob(DIR_NAME)
+    for d in dir:
+        filelog.append(d)
+
+    print(filelog)
+    return filelog
 
 if __name__ == "__main__":
     startdate = sys.argv[1]
